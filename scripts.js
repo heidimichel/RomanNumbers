a12b18490ac59cfc4f3a5187cc0ed989a526525d
Heidi Michel <heidi_michel@hotmail.com>
2022-08-15 17:47:55 +0200

    Updates to conversion code
    Switch case works partially, proper RegEx is required to make any roman number (up to 9999) convertible in my code.

--- scripts.js
+++ scripts.js
@@ -7,11 +7,38 @@ const getValueInput = () => {
     case "I":
       text = "1";
       break;
+    case "II":
+      text = "2";
+      break;
+    case "III":
+      text = "3";
+      break;
+    case "IV":
+      text = "4";
+      break;
     case "V":
       text = "5";
       break;
+    case "VI":  
+      text = "6";
+      break;
+    case "VII":  
+      text = "7";
+      break;
+    case "VIII":  
+      text = "8";
+    break;
+    case "IX":  
+      text = "9";
+      break;
     case "X":
       text = "10";
+      break;
+    case "XX":
+      text = "20";
+      break;
+    case "XXX":
+      text = "30";
       break;
     case "L":
       text = "50";
@@ -19,14 +46,20 @@ const getValueInput = () => {
     case "C":
       text = "100";
       break;
+    case "CC":
+      text = "200";
+      break;
+    case "CCC":
+      text = "300";
+      break;
     case "D":
       text = "500";
       break;
     case "M":
       text = "1000";
       break;
-    case "II":
-      text = "2";
+    case "MM":
+      text = "2000";
       break;
     default:
       text = "Not a valid roman number";
